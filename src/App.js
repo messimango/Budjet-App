ee9125a68aae547c2f5d05c21eef7c9668c5679d
Umang Poudyal <umangpoudyal@gmail.com>
2022-04-27 18:05:04 -0400

    Changed layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import {Stack, Button} from "react-bootstrap";
+import './App.css';
+
 function App() {
-  return "I Love Turtles"
+  return <div className="mx-5 mt-3">
+    <h1 className="title d-flex justify-content-center mb-5">Budget App</h1>
+    <Stack direction='horizontal' gap='3' classname='header'>      
+      <Button variant="success">Add Budget</Button>
+      <Button variant="warning">Add Expense</Button>
+    </Stack>
+
+    <div className="boxes">
+      
+    </div>
+
+  </div>
 }
 
 export default App;
